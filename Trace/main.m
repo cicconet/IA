5ba276fc2a9f87545cc65608bcacdf9a9da631d3
Cicconet <mc457@idac-wkstn4.med.harvard.edu>
2016-08-11 11:42:45 -0400

    ...

--- Trace/main.m
+++ Trace/main.m
@@ -5,7 +5,7 @@ end
 
 thr1 = 0.2;
 thr2 = 0.1;
-[trs, RGB, DRGB, BW] = trace(I,thr1,thr2);
+[trs, RGB, DRGB, BW] = traceoutlines(I,thr1,thr2);
 
 Out = [RGB DRGB; repmat(I,[1 1 3]) repmat(BW,[1 1 3])];
 imshow(Out);
