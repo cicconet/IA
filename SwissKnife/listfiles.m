a44dea92820ddf5ed6bd5f44ba49a25730603e4f
Cicconet <mc457@idac-wkstn4.med.harvard.edu>
2016-11-08 10:07:51 -0500

    ...

--- SwissKnife/listfiles.m
+++ SwissKnife/listfiles.m
@@ -1,12 +1,15 @@
-function list = listfiles(folderpath, fileextension)
-% returns a cell array with the full filepaths of files ending with 'fileextension' in folder 'folderpath'
+function [fplist,fnlist] = listfiles(folderpath, fileextension)
+% returns cell arrays with the filepaths/filenames of files ending with 'fileextension' in folder 'folderpath'
 % fileextension examples: '.tif', '.png', '.txt'
+% fplist: list of full paths
+% fnlist: list of file names
 listing = dir(folderpath);
 index = 0;
 for i = 1:size(listing,1)
     s = listing(i).name;
     if ~isempty(strfind(s,fileextension))
         index = index+1;
-        list{index} = [folderpath filesep s];
+        fplist{index} = [folderpath filesep s];
+        fnlist{index} = s;
     end
 end
